f93be92a33cde71a24bb117067a538e61ede1558
asabla <asabla@users.noreply.github.com>
2023-06-27 02:53:53 +0200

    Removed no longer needed example query
    Just keeping things mean and clean

--- src/Backend/BattleBitGraphQLApi/GraphQL/Query.cs
+++ src/Backend/BattleBitGraphQLApi/GraphQL/Query.cs
@@ -4,24 +4,4 @@ namespace BattleBitProxy.Backend.BattleBitGraphQLApi.GraphQL;
 [GraphQLDescription("All supported queries")]
 public class Query
 {
-    public Book GetBook() =>
-        new()
-        {
-            Title = "Some example Book title",
-            Author = new()
-            {
-                Name = "Some example Author Name"
-            }
-        };
-}
-
-public record Book
-{
-    public string Title { get; set; } = null!;
-    public Author Author { get; set; } = null!;
-}
-
-public record Author
-{
-    public string Name { get; set; } = null!;
 }
